3421fea52820235be19884382bcc675c37be3e03
Jonathan Hsieh <jonathanh1386@gmail.com>
2019-09-06 09:47:17 -0700

    cleaned up eval mod

--- evaluate.py
+++ evaluate.py
@@ -8,6 +8,17 @@ def checkBound(board, oriCord, trans):
 
     return True
 
+def checkTransform(board, transform, pos):
+    rtn =  0
+    for t in transform:
+        if(not checkBound(board, pos, t)):
+            break
+
+        if(board.state[pos[0] + t[0]][pos[1] + t[1]] == board.state[pos[0]][pos[1]]):
+            rtn += 1
+        else:
+            break
+    return rtn
 
 def countConnected(board): # returns an array [[a1, b1, c1], [a2, b2, c3]] with connected [2, 3, 4]
     rtnCount = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
@@ -25,52 +36,10 @@ def countConnected(board): # returns an array [[a1, b1, c1], [a2, b2, c3]] with 
             if(board.state[i][j] == -1):
                 continue
 
-            hrtMatch = 0
-            for t in hort:
-                if(not checkBound(board, [i, j], t)):
-                    break
-
-                if(board.state[i + t[0]][j + t[1]] == board.state[i][j]):
-                    hrtMatch += 1
-                else:
-                    break
-            rtnCount[board.state[i][j]][hrtMatch] += 1
-
-            vrtMatch = 0
-            for t in vert:
-
-                if(not checkBound(board, [i, j], t)):
-                    break
-
-                if(board.state[i + t[0]][j + t[1]] == board.state[i][j]):
-                    vrtMatch += 1
-                else:
-                    break
-
-            rtnCount[board.state[i][j]][vrtMatch] += 1
-
-            tdiaMatch = 0
-            for t in tdiag:
-                if(not checkBound(board, [i, j], t)):
-                    break
-
-                if(board.state[i + t[0]][j + t[1]] == board.state[i][j]):
-                    tdiaMatch += 1
-                else:
-                    break
-            rtnCount[board.state[i][j]][tdiaMatch] += 1
-
-            bdiaMatch = 0
-            for t in bdiag:
-
-                if(not checkBound(board, [i, j], t)):
-                    break
-
-                if(board.state[i + t[0]][j + t[1]] == board.state[i][j]):
-                    bdiaMatch += 1
-                else:
-                    break
-            rtnCount[board.state[i][j]][bdiaMatch] += 1
+            rtnCount[board.state[i][j]][checkTransform(board, hort, [i, j])] += 1
+            rtnCount[board.state[i][j]][checkTransform(board, vert, [i, j])] += 1
+            rtnCount[board.state[i][j]][checkTransform(board, tdiag, [i, j])] += 1
+            rtnCount[board.state[i][j]][checkTransform(board, bdiag, [i, j])] += 1
 
     return rtnCount
 
